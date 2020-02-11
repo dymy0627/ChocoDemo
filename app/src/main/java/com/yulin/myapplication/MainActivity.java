557703116b6b8860aa38938c390fb7f520b3e4d7
AndyLiu <dymy0627@hotmail.com>
2020-02-11 22:43:42 +0800

    Impelement network exception toast and pulldown to refresh

--- app/src/main/java/com/yulin/myapplication/MainActivity.java
+++ app/src/main/java/com/yulin/myapplication/MainActivity.java
@@ -10,18 +10,21 @@ import android.util.Log;
 import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
 
 import com.yulin.myapplication.database.ChocoDatabase;
 import com.yulin.myapplication.web.ChocoService;
 import com.yulin.myapplication.web.DramaResponse;
 import com.yulin.myapplication.web.RetrofitServiceManager;
 
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 import io.reactivex.Single;
 import io.reactivex.SingleObserver;
 import io.reactivex.android.schedulers.AndroidSchedulers;
@@ -44,6 +47,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
+        mSharedPreferences = getSharedPreferences("Choco", MODE_PRIVATE);
 
         RecyclerView recyclerView = findViewById(R.id.drama_listView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -85,6 +90,40 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mDramaNameList);
         mSearchEditText.setAdapter(adapter);
         mSearchEditText.setThreshold(1);
+
+        final SwipeRefreshLayout refreshView = findViewById(R.id.refreshView);
+        refreshView.setOnRefreshListener(() -> {
+            Log.d(TAG, "onRefresh");
+            getDramaListFromWeb()
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(new SingleObserver<List<DramaBean>>() {
+                        @Override
+                        public void onSubscribe(Disposable d) {
+
+                        }
+
+                        @Override
+                        public void onSuccess(List<DramaBean> dramaBeans) {
+                            updateDramaList(dramaBeans);
+                            updateAutoCompleteData(dramaBeans);
+                            Toast.makeText(MainActivity.this, "列表更新完成", Toast.LENGTH_SHORT).show();
+                            refreshView.setRefreshing(false);
+                        }
+
+                        @Override
+                        public void onError(Throwable e) {
+                            Log.d(TAG, "onError:");
+                            e.printStackTrace();
+                            if (e instanceof UnknownHostException) {
+                                Toast.makeText(MainActivity.this, "請確認網路連線", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(MainActivity.this, "列表更新異常", Toast.LENGTH_SHORT).show();
+                            }
+                            refreshView.setRefreshing(false);
+                        }
+                    });
+        });
     }
 
     private SharedPreferences mSharedPreferences;
@@ -94,38 +133,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         Log.d(TAG, "onStart");
-        getDramaList().subscribe(new SingleObserver<List<DramaBean>>() {
 
-            @Override
-            public void onSubscribe(Disposable d) {
-                Log.d(TAG, "onSubscribe:");
-            }
+        getDramaList()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new SingleObserver<List<DramaBean>>() {
 
-            @Override
-            public void onSuccess(List<DramaBean> dramaBeans) {
-                if (!dramaBeans.isEmpty()) {
-                    mDramaBeanList.clear();
-                    mDramaBeanList.addAll(dramaBeans);
-                    mViewAdapter.notifyDataSetChanged();
-
-                    for (DramaBean dramaBean : dramaBeans) {
-                        mDramaNameList.add(dramaBean.getName());
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        Log.d(TAG, "onSubscribe:");
                     }
 
-                    mSharedPreferences = getSharedPreferences("Choco", MODE_PRIVATE);
-                    String lastSearchText = mSharedPreferences.getString(LAST_SEARCH, null);
-                    if (lastSearchText != null && !lastSearchText.isEmpty()) {
-                        mSearchEditText.setText(lastSearchText);
-                    }
-                }
-            }
+                    @Override
+                    public void onSuccess(List<DramaBean> dramaBeans) {
+                        if (!dramaBeans.isEmpty()) {
+                            updateDramaList(dramaBeans);
+                            updateAutoCompleteData(dramaBeans);
 
-            @Override
-            public void onError(Throwable t) {
-                Log.d(TAG, "onError:");
-                t.printStackTrace();
-            }
-        });
+                            // restore last search state.
+                            String lastSearchText = mSharedPreferences.getString(LAST_SEARCH, null);
+                            if (lastSearchText != null && !lastSearchText.isEmpty()) {
+                                mSearchEditText.setText(lastSearchText);
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onError(Throwable t) {
+                        Log.d(TAG, "onError:");
+                        if (t instanceof UnknownHostException) {
+                            Toast.makeText(MainActivity.this, "請確認網路連線", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(MainActivity.this, "獲取列表異常", Toast.LENGTH_SHORT).show();
+                        }
+                        t.printStackTrace();
+                    }
+                });
     }
 
     @Override
@@ -133,6 +176,20 @@ public class MainActivity extends AppCompatActivity {
         super.onStop();
         Log.d(TAG, "onStop");
         mSharedPreferences.edit().putString(LAST_SEARCH, mSearchEditText.getText().toString()).apply();
+    }
+
+    private void updateDramaList(List<DramaBean> dramaBeans) {
+        mDramaBeanList.clear();
+        mDramaBeanList.addAll(dramaBeans);
+        mViewAdapter.notifyDataSetChanged();
+    }
+
+    // update data for AutoCompleteTextView.
+    private void updateAutoCompleteData(List<DramaBean> dramaBeans) {
+        mDramaNameList.clear();
+        for (DramaBean dramaBean : dramaBeans) {
+            mDramaNameList.add(dramaBean.getName());
+        }
     }
 
     public Single<List<DramaBean>> getDramaList() {
@@ -143,9 +200,7 @@ public class MainActivity extends AppCompatActivity {
                     } else {
                         return Single.just(dramaList);
                     }
-                })
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread());
+                });
     }
 
     public Single<List<DramaBean>> getDramaListFromWeb() {
